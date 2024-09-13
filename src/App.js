c4f37559d47904fc0af4a280e925a68446b1662c
ebusten <eric.s.busten@gmail.com>
2024-09-13 12:11:38 -0300

    prueba test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save CUARTA PRUEBA.
+          Edit <code>src/App.js</code> and save QUINTA PRUEBA.
         </p>
         <a
           className="App-link"
